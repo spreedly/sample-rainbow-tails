c166cac88b962de517c7c4c4e002b9c4aa81b9fb
Duff OMelia <duff@omelia.org>
2013-07-27 18:20:47 -0400

    Perhaps we'll call it spreedly for now

--- app/controllers/tails_controller.rb
+++ app/controllers/tails_controller.rb
@@ -1,6 +1,6 @@
 class TailsController < ApplicationController
 
-  helper_method :environment
+  helper_method :spreedly
   rescue_from Spreedly::Error, with: :handle_spreedly_error
 
   def buy_tail
@@ -10,10 +10,10 @@ class TailsController < ApplicationController
   def transparent_redirect_complete
     return if error_saving_card
 
-    @credit_card = CreditCard.new(environment.find_payment_method(params[:token]))
+    @credit_card = CreditCard.new(spreedly.find_payment_method(params[:token]))
     return render_buy_tail unless @credit_card.valid?
 
-    transaction = environment.purchase_on_gateway(gateway_token, @payment_method_token, amount_to_charge)
+    transaction = spreedly.purchase_on_gateway(gateway_token, @payment_method_token, amount_to_charge)
     return render_unable_to_process(transaction) unless transaction.succeeded?
 
     return redirect_to(successful_purchase_url)
@@ -38,8 +38,8 @@ class TailsController < ApplicationController
     render(:action => :buy_tail)
   end
 
-  def environment
-    @environment ||= Spreedly::Environment.new(ENV["CORE_ENVIRONMENT_KEY"], ENV["CORE_ACCESS_SECRET"])
+  def spreedly
+    @spreedly ||= Spreedly::Environment.new(ENV["CORE_ENVIRONMENT_KEY"], ENV["CORE_ACCESS_SECRET"])
   end
 
   def amount_to_charge
